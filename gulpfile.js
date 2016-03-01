57a32ac3ea5ddba75755fc09b6410844a5c85c54
Andrzejek <belion1@wp.pl>
2016-03-01 11:34:50 +0100

    more gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var watch = require('gulp-watch');
 // var paths = require('gulp-path');
 //var rename = require('gulp-rename');
 var build = require('gulp-build');
+var bootstrap = require('bootstrap')
 
 
 // define tasks here
@@ -20,21 +21,26 @@ gulp.task('html', function() {
         .pipe(gulp.dest('dist'));
 });
 
+gulp.task('js', function() {
+    return gulp.src('app/*.js')
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('imgs', function() {
+    return gulp.src('app/img/**')
+        .pipe(gulp.dest('dist/img'));
+});
+
 gulp.task('watch', function() {
     // gulp.watch('app/scripts/**/*.js', ['scripts']);
     gulp.watch('app/styles/**/*.scss', ['sass']);
     gulp.watch('app/*.html', ['html']);
 });
 
-
-gulp.task('build', function() {
-    return gulp.src('app/styles/**/*.scss','app/index.html')
-        .pipe(build({ GA_ID: '123456' }))
-        .pipe(gulp.dest('dist'))
-});
+gulp.task('build', ['sass','html', 'js', 'imgs']);
 
 gulp.task('default', function() {
     // run tasks here
     // set up watch handlers here
-    
+
 });
