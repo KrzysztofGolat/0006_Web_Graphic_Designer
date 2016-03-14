23b2fd3c65213e178e952e197d60e75169683fca
KrzysztofGolat <kgolat@gmail.com>
2016-03-14 18:51:01 +0100

    Favicon, paddings in all sections.

--- app/scripts/sticky.js
+++ app/scripts/sticky.js
@@ -17,4 +17,17 @@ $(document).ready(function() {
     $(window).on('scroll resize', function() {
         stickyNav();
     });
+	
+	var img-responsive-magni = function () {
+        if $('img-responsive').hasClass('hover') { 
+            $('.img-responsive').addClass('img-magnifier');
+            } else {
+                $('.img-responsive').removeClass('img-magnifier'); 
+            }
+        };
+ 
+    img-responsive-magni ();
+
+    	
+	
 });
