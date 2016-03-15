8bffee090b572b9a6e56ff52b5a033208af2809f
KrzysztofGolat <kgolat@gmail.com>
2016-03-15 17:08:24 +0100

    My skills bars.

--- app/scripts/sticky.js
+++ app/scripts/sticky.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var stickyNav = function(){
         var stickyNavTop = $('.header').height(); // $('.navbar').offset().top;
         var scrollTop = $(window).scrollTop();
-        console.log(stickyNavTop, scrollTop);
+        // console.log(stickyNavTop, scrollTop);
 
         if (scrollTop > stickyNavTop) { 
             $('.navbar').addClass('sticky');
@@ -14,20 +14,24 @@ $(document).ready(function() {
  
     stickyNav();
 
+    // $(window).on('scroll resize', function() {
+    //     stickyNav();
+
+
+    var showBar = function(){
+        var showBarTop = $('.myskills__bar').first().offset().top; // $('.navbar').offset().top;
+        var scrollTop = $(window).scrollTop();
+//        console.log(scrollTop,  showBarTop - $(window).height() * 0.90);
+
+        if (scrollTop > showBarTop - $(window).height()) { 
+            $('.myskills__bar').removeClass('initial');
+        } 
+    };
+ 
+    showBar();
+
     $(window).on('scroll resize', function() {
+        showBar();
         stickyNav();
     });
-	
-	var img-responsive-magni = function () {
-        if $('img-responsive').hasClass('hover') { 
-            $('.img-responsive').addClass('img-magnifier');
-            } else {
-                $('.img-responsive').removeClass('img-magnifier'); 
-            }
-        };
- 
-    img-responsive-magni ();
-
-    	
-	
 });
