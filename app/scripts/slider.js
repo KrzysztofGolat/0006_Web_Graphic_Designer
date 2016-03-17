7ea3130a0315f5294972c7192845b85c363c444a
KrzysztofGolat <kgolat@gmail.com>
2016-03-17 18:18:01 +0100

    Slider the beginning stage 2 :)

---
+++ app/scripts/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    var sliderimage1 = function () {
+    $('.slider__circle1').click(function() {
+        $('.header').addClass('slider1')
+        
+    
+        
+    });
+
+    };
+ });
