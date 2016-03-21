9b23908db1847fbf06fb78217d42bcbd230c2450
KrzysztofGolat <kgolat@gmail.com>
2016-03-21 20:41:54 +0100

    Top slider.

--- app/scripts/slider.js
+++ app/scripts/slider.js
@@ -11,6 +11,27 @@ $(document).ready(function() {
         $('.sliderbox__sdot').removeClass('bgr');
         $(this).addClass('bgr');
     });
+    
+    
+    $('.carousel-control.right').click(function(){
+        var $positionN = $topSlider.bgswitcher("next");
+        $('.sliderbox__sdot.bgr').removeClass('bgr').next().addClass('bgr');
+        if ($('.sliderbox__sdot.bgr').length == 0) {
+            $('.sliderbox__sdot:first').addClass('bgr');
+            $topSlider.bgswitcher("select", 0);
+        }
+    });
+
+    $('.carousel-control.left').click(function(){
+        var $positionEx = $topSlider.bgswitcher("prev");
+        $('.sliderbox__sdot.bgr').removeClass('bgr').prev().addClass('bgr');
+        if ($('.sliderbox__sdot.bgr').length == 0) {
+            $('.sliderbox__sdot:last').addClass('bgr');
+            $topSlider.bgswitcher("select", 2);
+        }
+    });
+    
+    
 
     var myLoop = function() {
         // przejdz do next slide
@@ -18,11 +39,11 @@ $(document).ready(function() {
         // przejdz do next kropka
         $('.sliderbox__sdot.bgr').removeClass('bgr').next().addClass('bgr');
         if ($('.sliderbox__sdot.bgr').length == 0) {
-            $('.sliderbox__sdot:first').addClass('bgr');
+            $('.sliderbox__sdot:last').addClass('bgr');
             $topSlider.bgswitcher("select", 0);
         }
         // ustaw timeout na nowo
-        setTimeout(myLoop, 5000);
+//        setTimeout(myLoop, 5000);
     };
-    setTimeout(myLoop, 5000);
+//    setTimeout(myLoop, 5000);
 });
